c7d108713069a9ae56d4ba947d6d2c95e35105d7
Daniel <daniel.martos@hotmail.com>
2025-06-07 22:13:00 +0200

    fix

--- src/lib/constants/links.ts
+++ src/lib/constants/links.ts
@@ -1,4 +1,5 @@
 //Localhost
-export const resetPasswordLink = "http://localhost:8081/auth/reset-password";
+export const resetPasswordLink =
+  "https://barber-backend-vhfs.vercel.app/auth/reset-password";
 // export const resetPasswordLink = "barberapp:///auth/reset-password";
 //Produccion
