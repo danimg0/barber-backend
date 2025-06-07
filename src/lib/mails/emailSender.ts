d05f16673a29d6f0c9ef9c76d1708a889b960032
Daniel <daniel.martos@hotmail.com>
2025-06-08 01:40:09 +0200

    email

--- src/lib/mails/emailSender.ts
+++ src/lib/mails/emailSender.ts
@@ -5,10 +5,9 @@ const SMTP_SERVER_USERNAME = process.env.SMTP_SERVER_USERNAME;
 const SMTP_SERVER_PASSWORD = process.env.SMTP_SERVER_PASSWORD;
 const SITE_MAIL_RECIEVER = process.env.SITE_MAIL_RECIEVER;
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: SMTP_SERVER_HOST,
-  port: 587,
-  secure: false,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: SMTP_SERVER_USERNAME,
     pass: SMTP_SERVER_PASSWORD,
@@ -30,25 +29,20 @@ export async function sendMail({
 }) {
   try {
     const isVerified = await transporter.verify();
+    const info = await transporter.sendMail({
+      from: `"Web Contact" <${SMTP_SERVER_USERNAME}>`,
+      to: sendTo || SITE_MAIL_RECIEVER,
+      subject,
+      text,
+      html: html ?? "",
+      replyTo: email,
+    });
+    console.log("Message Sent", info.messageId);
+    return info;
   } catch (error) {
-    console.error(
-      "Something Went Wrong",
-      SMTP_SERVER_USERNAME,
-      SMTP_SERVER_PASSWORD,
-      error
-    );
-    return;
+    console.error("Error al enviar correo:", error);
+    return { error };
   }
-  const info = await transporter.sendMail({
-    from: email,
-    to: sendTo || SITE_MAIL_RECIEVER,
-    subject: subject,
-    text: text,
-    html: html ? html : "",
-  });
-  console.log("Message Sent", info.messageId);
-  console.log("Mail sent to", SITE_MAIL_RECIEVER);
-  return info;
 }
 
 // const nodemailer = require("nodemailer");
