e00a627008d2c6b161c225c75af0898227e37a9b
Daniel <daniel.martos@hotmail.com>
2025-06-07 22:15:41 +0200

    cors fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const allowedOrigins = [
-  // cuidado que esta me va cambiando con cada push del main
-  "https://vercel.com/danis-projects-5aeb92e7/barber-backend-vhfs/8E8Lqt9snCNrWag984oVhW2hudyD",
-  "http://localhost:8081",
-  "http://192.168.1.148:8081",
-];
+// const allowedOrigins = [
+// cuidado que esta me va cambiando con cada push del main
+// "https://vercel.com/danis-projects-5aeb92e7/barber-backend-vhfs/8E8Lqt9snCNrWag984oVhW2hudyD",
+// "http://localhost:8081",
+// "http://192.168.1.148:8081",
+// ];
 
 const corsOptions = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
@@ -15,7 +15,7 @@ const corsOptions = {
 export function middleware(request: NextRequest) {
   // Check the origin from the request
   const origin = request.headers.get("origin") ?? "";
-  const isAllowedOrigin = allowedOrigins.includes(origin);
+  // const isAllowedOrigin = allowedOrigins.includes(origin);
 
   // const isAllowedOrigin = true; // For simplicity, allow all origins in this example
   // const origin = request.headers.get("origin") || "*"; // Default to '*' if no origin is provided
@@ -25,7 +25,8 @@ export function middleware(request: NextRequest) {
 
   if (isPreflight) {
     const preflightHeaders = {
-      ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
+      // ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
+      "Access-Control-Allow-Origin": "*",
       ...corsOptions,
     };
     return NextResponse.json({}, { headers: preflightHeaders });
@@ -34,9 +35,10 @@ export function middleware(request: NextRequest) {
   // Handle simple requests
   const response = NextResponse.next();
 
-  if (isAllowedOrigin) {
-    response.headers.set("Access-Control-Allow-Origin", origin);
-  }
+  // if (isAllowedOrigin) {
+  // response.headers.set("Access-Control-Allow-Origin", origin);
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  // }
 
   Object.entries(corsOptions).forEach(([key, value]) => {
     response.headers.set(key, value);
